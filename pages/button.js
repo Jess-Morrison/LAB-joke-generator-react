68457983ebb1421e1b94ff497f19581136b75370
Jessica Morrison <jnicomorr@gmail.com>
2022-07-11 21:37:09 -0500

    Cont to work on ternary

--- pages/button.js
+++ pages/button.js
@@ -7,10 +7,15 @@ export default function Button() {
   const newJoke = () => {
     getJoke().then((jokeObj) => setJoke(jokeObj.setup));
   };
+  const jokeDelivery = () => {
+    getJoke().then((jokeObj) => setJoke(jokeObj.delivery));
+  };
   return (
     <div>
       <h3>{ joke } </h3>
-      <button type="submit" onClick={newJoke}> Tell me a Joke!</button>
+
+      {newJoke()
+        ? <button type="submit" onClick={newJoke}> Tell me a Joke!</button> : <button type="submit" onClick={jokeDelivery}> Get Punchline!</button>}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Button from './button';
+
 function Home() {
   return (
     <div
@@ -10,6 +12,7 @@ function Home() {
       }}
     >
       <h1>Welcome Home!</h1>
+      <Button />
     </div>
   );
 }
