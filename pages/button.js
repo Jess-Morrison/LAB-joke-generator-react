585914c205aa1bc3255a48e4a617de8507544a7d
Jessica Morrison <jnicomorr@gmail.com>
2022-07-12 21:35:01 -0500

    Still working on ternary

--- pages/button.js
+++ pages/button.js
@@ -3,19 +3,25 @@ import getJoke from '../api/jokeData';
 
 export default function Button() {
   const [joke, setJoke] = useState();
+  const [delivery, setDelivery] = useState();
 
   const newJoke = () => {
+    console.warn(getJoke());
     getJoke().then((jokeObj) => setJoke(jokeObj.setup));
   };
+  console.warn(joke);
+  console.warn(delivery);
   const jokeDelivery = () => {
-    getJoke().then((jokeObj) => setJoke(jokeObj.delivery));
+    getJoke().then((jokeObj) => setDelivery(jokeObj.delivery));
   };
   return (
     <div>
-      <h3>{ joke } </h3>
+      {/* <h3>joke ? {setup ? joke.setup : joke.delivery} : '' </h3> */}
+      <h3> {joke || delivery}  </h3>
 
-      {newJoke()
-        ? <button type="submit" onClick={newJoke}> Tell me a Joke!</button> : <button type="submit" onClick={jokeDelivery}> Get Punchline!</button>}
+      {joke
+        ? <button type="submit" onClick={jokeDelivery}> Get Punchline!</button>
+        : <button type="submit" onClick={newJoke}> Tell me a Joke!</button>}
     </div>
   );
 }
