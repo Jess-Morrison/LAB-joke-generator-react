244cf0fe046feed4d05ad34de9a7eaf25b7594f7
Jessica Morrison <jnicomorr@gmail.com>
2022-07-15 22:58:48 -0500

    Getting closer

--- pages/button.js
+++ pages/button.js
@@ -2,26 +2,31 @@ import React, { useState } from 'react';
 import getJoke from '../api/jokeData';
 
 export default function Button() {
-  const [joke, setJoke] = useState();
+  const [joke, setJoke] = useState('Tell me a Joke!');
   const [delivery, setDelivery] = useState();
 
   const newJoke = () => {
-    console.warn(getJoke());
-    getJoke().then((jokeObj) => setJoke(jokeObj.setup));
-  };
-  console.warn(joke);
-  console.warn(delivery);
-  const jokeDelivery = () => {
-    getJoke().then((jokeObj) => setDelivery(jokeObj.delivery));
+    if (joke === 'Tell me a Joke!') {
+      getJoke().then((jokeObj) => setJoke(jokeObj.setup));
+    // eslint-disable-next-line brace-style
+    }
+    // && setDelivery(jokeObj.delivery));
+    else if (delivery === 'Get punchline!') {
+      // eslint-disable-next-line no-lone-blocks
+      { getJoke().then((jokeObj) => setDelivery(jokeObj.delivery)); }
+    }
   };
   return (
+    // eslint-disable-next-line react/jsx-no-comment-textnodes
     <div>
-      {/* <h3>joke ? {setup ? joke.setup : joke.delivery} : '' </h3> */}
-      <h3> {joke || delivery}  </h3>
+      <h3> {joke} </h3>
 
-      {joke
-        ? <button type="submit" onClick={jokeDelivery}> Get Punchline!</button>
+      { joke
+        ? <button type="submit" onClick={newJoke}> Get Punchline!</button>
         : <button type="submit" onClick={newJoke}> Tell me a Joke!</button>}
+      {/* <h3>{ delivery } </h3> */}
+      {/* { delivery
+        ? <button type="submit" onClick={jokeDelivery}> Tell me a Joke!</button> : ''} */}
     </div>
   );
 }
